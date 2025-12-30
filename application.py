2d8762f6c6996f0a5667c5bc004efd6f58c69bed
radiation-hazard <kunal54431zz@gmail.com>
2025-12-30 10:39:12 +0530

    fix typo

--- application.py
+++ application.py
@@ -4,8 +4,8 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-appllication = Flask(__name__)
-app = appllication
+application = Flask(__name__)
+app = application
 
 ## import ridge regressor and standard scaler pickle 
 ridge_model = pickle.load(open('models/ridge.pkl','rb'))
